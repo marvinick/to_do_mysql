52d59a9b6573f709bc805d5085e9d24f1fcdd6eb
Jason Bethel & Annie Hoogendoorn <student@epicodus.com>
2015-08-05 14:41:03 -0700

    clean task root path

--- app/app.php
+++ app/app.php
@@ -21,14 +21,11 @@
     });
 
 
-    $app->post("/tasks", function() {
+    $app->post("/tasks", function() use ($app) {
           $task = new Task($_POST['description']);
           $task->save();
-          return "
-              <h1>You created a task!</h1>
-              <p>" . $task->getDescription() . "</p>
-              <p><a href='/'>View your list of things to do.</a></p>
-              ";
+          return $app ['twig']->render('create_task.html.twig', array('newtask' => $task));
+
     });
 
     $app->post("/delete_tasks", function() {
--- views/create_task.html.twig
+++ views/create_task.html.twig
@@ -4,5 +4,7 @@
 </head>
 <body>
     <h1>You created a task!</h1>
+    <p>{{ newtask.getDescription }}</p>
+    <p><a href='/'>View your list of things to do.</a></p>
 </body>
 </html>
