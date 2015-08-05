edc1311176d1ca230d790c8294bb367695e2871f
Jason Bethel & Annie Hoogendoorn <student@epicodus.com>
2015-08-05 10:45:46 -0700

    update app.php with 3 Task object instances

--- app/app.php
+++ app/app.php
@@ -4,6 +4,22 @@
 
     $app = new Silex\Application();
 
+    $app->get("/", function() {
+        $test_task = new Task("Learn PHP.");
+        $another_test_task = new Task("Learn Drupal.");
+        $third_task = new Task("Visit France.");
+
+        $list_of_tasks = array($test_task, $another_test_task);
+        $output = "";
+
+        foreach ($list_of_tasks as $task) {
+            $output = $output . "<p>" . $task->getDescription() . "</p>";
+        }
+
+    return $output;
+
+  });
+  
     return $app;
 
 ?>
