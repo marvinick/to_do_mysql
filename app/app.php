51ea4affacba208d8d1bcd4816fb32451795a3de
Mike Chastain & Jason Bethel <student@epicodus.com>
2015-08-17 14:51:17 -0700

    add paths to categories.html.twig in the app.php file

--- app/app.php
+++ app/app.php
@@ -50,5 +50,16 @@
         return $app['twig']->render('index.html.twig');
     });
 
+    $app->post("/categories", function() use ($app) {
+        $category = new Category($_POST['name']);
+        $category->save();
+        return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+    });
+
+    $app->post("/delete_categories", function() use ($app) {
+        Category::deleteAll();
+        return $app['twig']->render('index.html.twig');
+    });
+
     return $app;
 ?>
