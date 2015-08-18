a07e2f6c3b830a9367c6449977fc44b21ba0ce9f
Mike Chastain & Marvin Nikijuluw <student@epicodus.com>
2015-08-18 12:20:15 -0700

    it will add silex, routes, twig

--- app/app.php
+++ app/app.php
@@ -20,7 +20,7 @@
 
     $app->get("/", function() use ($app) {
 
-        return $app['twig']->render('index.html.twig');
+        return $app['twig']->render('index.html.twig', array('categories' => Category::getAll()));
 
     });
 
@@ -38,27 +38,36 @@
     });
 
     $app->post("/tasks", function() use ($app) {
-        $task = new Task($_POST['description']);
+        $description = $_POST['description'];
+        $category_id = $_POST['category_id'];
+
+        $task = new Task($description, $id = null, $category_id);
         $task->save();
-        return $app ['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
+        $category = Category::find($category_id);
+        return $app ['twig']->render('category.html.twig', array('category' => $category, 'tasks' => Task::getAll()));
 
     });
 
 
     $app->post("/delete_tasks", function() use ($app) {
         Task::deleteAll();
-        return $app['twig']->render('index.html.twig');
+        return $app['twig']->render('category.html.twig');
     });
 
     $app->post("/categories", function() use ($app) {
         $category = new Category($_POST['name']);
         $category->save();
-        return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+        return $app['twig']->render('index.html.twig', array('categories' => Category::getAll()));
     });
 
     $app->post("/delete_categories", function() use ($app) {
         Category::deleteAll();
         return $app['twig']->render('index.html.twig');
+    });
+
+    $app->get("/categories/{id}", function($id) use ($app) {
+        $category = Category::find($id);
+        return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => $category->getTasks()));
     });
 
     return $app;
